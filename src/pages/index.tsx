ba79ff56dc78c34f4ee67a5130ffa489bdcb1129
Vetle de Vries <vetle1608@gmail.com>
2023-01-19 11:33:40 +0100

    twmerge+clsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head"
 import { BitcoinResData, bitcoinResponse } from "../utils/zod/schemas"
 import { z } from "zod"
 import React, { TableHTMLAttributes, useState, type ReactNode } from "react"
+import { cn } from "../utils/cn"
+import { ClassValue } from "clsx"
 
 interface WithChildren {
   children: ReactNode | ReactNode[]
@@ -74,20 +76,48 @@ const objectKeysToArray = <T extends object>(object: T) => {
 //   )
 // }
 
-interface TableRowProps extends TableHTMLAttributes<HTMLTableRowElement> {
-  classNames?: string
+const PaginationButtons = ({
+  page,
+  pageLimit,
+  totalLength,
+  handleClick,
+}: {
+  page: number
+  pageLimit: number
+  totalLength: number
+  handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+}) => {
+  const totalPages = Math.ceil(totalLength / pageLimit)
+  return (
+    <div className='flex gap-2'>
+      {new Array(totalPages).fill("").map((_, i) => (
+        <button
+          value={i}
+          key={i}
+          onClick={handleClick}
+          className={`border border-slate-300 px-2 ${
+            page === i ? "bg-slate-200" : "bg-white"
+          }`}>
+          {i + 1}
+        </button>
+      ))}
+    </div>
+  )
 }
+
+interface TableRowProps extends TableHTMLAttributes<HTMLTableRowElement> {}
 
 const TableRow: React.FC<TableRowProps> = ({
   children,
-  classNames,
+  className,
   ...props
 }) => {
   return (
     <tr
-      className={
-        "grid grid-cols-7 gap-2 odd:bg-slate-50 even:bg-slate-200 [&>*]:border-l [&>*]:border-slate-300 [&>*]:p-2 first:[&>*]:border-none "
-      }>
+      className={cn(
+        "grid grid-cols-7 odd:bg-slate-50 even:bg-slate-200 [&>*]:border-l [&>*]:border-slate-300 [&>*]:p-2 first:[&>*]:border-none",
+        className,
+      )} {...props}>
       {children}
     </tr>
   )
@@ -98,8 +128,39 @@ const Home: NextPage = () => {
     queryKey: ["data.all"],
     queryFn: fetchData,
   })
+
   const [page, setPage] = useState(0)
   const [pageLimit, setPageLimit] = useState(20)
+
+  const SetPageActions = {
+    increment: "increment",
+    decrement: "decrement",
+  } as const
+
+  const handleSetPage = (action: ValuesOf<typeof SetPageActions>) => {
+    setPage((prevState) => {
+      if (action === SetPageActions.increment) {
+        return prevState + 1
+      } else if (action === SetPageActions.decrement) {
+        return prevState - 1
+      }
+      return prevState
+    })
+  }
+
+  const setOrdering = (key: KeysOf<BitcoinResData["Data"]["Data"]>) => {
+    if (!query.data) return
+    return query.data.Data.sort((a, b) => {
+      
+      console.log(a[key], b[key])
+      if (a[key] > b[key]) {
+        return 1
+      } else if (a[key] < b[key]) {
+        return -1
+      }
+      return 0
+
+    })}
 
   if (query.isLoading) return <div>Loading..</div>
   if (query.isError) return <div>Error</div>
@@ -111,59 +172,68 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <nav className='flex border-b bg-white p-4'>
+      {/* <nav className='flex border-b bg-white p-4'>
         <h1 className='text-4xl'>Bitcoin</h1>
-      </nav>
+      </nav> */}
 
       <main className='flex min-h-screen flex-col gap-4 bg-red-100 p-4'>
         {query.isLoading && <div>Loading..</div>}
-        <button
-          onClick={() => {
-            setPage((prevState) => {
-              if (prevState > 0) {
-                return prevState - 1
-              } else return prevState
-            })
-          }}>
-          BACK
-        </button>
-        <button onClick={() => setPage((prevState) => prevState + 1)}>
-          NEXT
-        </button>
         {query.data && (
-          <table className='flex flex-col overflow-hidden rounded-md bg-yellow-500'>
-            <thead>
-              <TableRow>
-                <th>High</th>
-                <th>Low</th>
-                <th>Time</th>
-                <th>Close</th>
-                <th>Open</th>
-                <th>Volumefrom</th>
-                <th>Volumeto</th>
-              </TableRow>
-            </thead>
+          <>
+            <div className='flex gap-2'>
+              <button
+                onClick={() => {
+                  setPage((prevState) => {
+                    if (prevState > 0) {
+                      return prevState - 1
+                    } else return prevState
+                  })
+                }}>
+                BACK
+              </button>
+              <PaginationButtons
+                page={page}
+                pageLimit={pageLimit}
+                totalLength={query.data.Data.length}
+                handleClick={(e) => setPage(parseInt(e.currentTarget.value))}
+              />
+              <button onClick={() => handleSetPage("increment")}>NEXT</button>
+            </div>
 
-            <tbody>
-              {query.data.Data.slice(
-                page * pageLimit,
-                page * pageLimit + pageLimit
-              ).map((day) => {
-                const dayKeysArr = objectKeysToArray(day)
-                const filtered = dayKeysArr.filter(
-                  (value) =>
-                    value !== "conversionSymbol" && value !== "conversionType"
-                )
-                return (
-                  <TableRow key={day.time}>
-                    {filtered.map((asd, i) => {
-                      return <td key={`${day[asd]}-${i}`}>{day[asd]}</td>
-                    })}
-                  </TableRow>
-                )
-              })}
-            </tbody>
-          </table>
+            <table className='flex flex-col overflow-hidden rounded-md bg-yellow-500'>
+              <thead>
+                <TableRow className='border-b table-fixed'>
+                  <th>High</th>
+                  <th>Low</th>
+                  <th>Time</th>
+                  <th>Close</th>
+                  <th>Open</th>
+                  <th>Volumefrom</th>
+                  <th>Volumeto</th>
+                </TableRow>
+              </thead>
+
+              <tbody>
+                {query.data.Data.slice(
+                  page * pageLimit,
+                  page * pageLimit + pageLimit
+                ).map((day) => {
+                  const dayKeysArr = objectKeysToArray(day)
+                  const filtered = dayKeysArr.filter(
+                    (value) =>
+                      value !== "conversionSymbol" && value !== "conversionType"
+                  )
+                  return (
+                    <TableRow key={day.time}>
+                      {filtered.map((asd, i) => {
+                        return <td key={`${day[asd]}-${i}`}>{day[asd]}</td>
+                      })}
+                    </TableRow>
+                  )
+                })}
+              </tbody>
+            </table>
+          </>
         )}
       </main>
     </>
